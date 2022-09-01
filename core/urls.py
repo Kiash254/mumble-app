e3cfe187c244b0bd9c2c93ff0f15b4a8913799f1
Kiash254 <codewithkiash@gmail.com>
2022-09-01 10:30:21 +0300

    added crud oparations

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import home,room
+from .views import home,room,CreateView,UpdateView,DeleteView
 
 app_name='core'
 
 urlpatterns = [
     path('', home, name='home'),
-    path('room/<int:pk>/',room,name='room')
+    path('room/<int:pk>/',room,name='room'),
+    path('create-room/',CreateView,name='create-room'),
+    path('update-room/<int:pk>/',UpdateView,name='update-room'),
+    path('delete-room/<int:pk>/',DeleteView,name='delete-room'),
 ]
